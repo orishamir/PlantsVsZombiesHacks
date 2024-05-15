13be004bf294e22b8a2b2d18518a013555b02f96
Ori Shamir <orishamir04@gmail.com>
2024-05-15 22:55:18 +0300

    Add lawnmower struct and enum

---
+++ models/lawnMower.cs
@@ -0,0 +1,26 @@
+﻿// ReSharper disable ArrangeThisQualifier
+namespace PlantsVsZombiesHacks.models;
+
+public struct LawnMower // size: 72 bytes
+{
+    public LawnmowerType LawnmowerType; // +0x34
+    public float DisplayPosY; // +0xC
+    public float DisplayPosX; // +0x8
+    public Byte IsDeleted; // +0x30
+
+    public LawnMower(LawnmowerType lawnmowerType, float displayPosY, float displayPosX, Byte isDeleted)
+    {
+        this.LawnmowerType = lawnmowerType;
+        this.DisplayPosY = displayPosY;
+        this.DisplayPosX = displayPosX;
+        this.IsDeleted = isDeleted;
+    }
+}
+
+public enum LawnmowerType
+{
+    Normal = 0,
+    PoolCleaner = 1,
+    RoofCleaner = 2,
+    Unidentifiable = 3,
+}
